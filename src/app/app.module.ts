1c9633a997a23e66094b2a670dd8220db71200e4
happy-sonday <mrsmiler1106@gmail.com>
2021-01-01 19:22:11 +0900

    sourcetree commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassicLayoutComponent } from './classic-layout/classic-layout.component';
+import { MediaLayoutComponent } from './media-layout/media-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassicLayoutComponent,
+    MediaLayoutComponent
   ],
   imports: [
     BrowserModule,
