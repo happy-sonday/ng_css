2f90f93d153f0d8a0a5a13c60dc78616eee9e67b
happy-sonday <mrsmiler1106@gmail.com>
2021-01-03 15:49:01 +0900

    font  practice

---
+++ src/app/font-test/font-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-test',
+  templateUrl: './font-test.component.html',
+  styleUrls: ['./font-test.component.scss']
+})
+export class FontTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
